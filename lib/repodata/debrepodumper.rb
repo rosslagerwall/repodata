545a36ff2781883c676e31795f6ad245e8b65d95
Ross Lagerwall <rosslagerwall@gmail.com>
2012-02-05 22:17:02 +0200

    Ignore invalid utf8 bytes in debian repo files.

--- lib/repodata/debrepodumper.rb
+++ lib/repodata/debrepodumper.rb
@@ -5,8 +5,12 @@ module Repodata
     private
     def dump_imp
       IO.foreach(@actual_fn) do |line|
-        print(line.strip[9..-1] + '_') if line =~ /^Package:/
-        puts(line.strip[9..-1]) if line =~ /^Version:/
+        begin
+          print(line.strip[9..-1] + '_') if line =~ /^Package:/
+          puts(line.strip[9..-1]) if line =~ /^Version:/
+        rescue ArgumentError
+          # sometimes conatins invalid UTF-8 bytes, ignore
+        end
       end
     end
   end
